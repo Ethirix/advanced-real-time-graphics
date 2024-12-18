c5a904a95dc52d95d5ef5bcfb8cb0156a1dcaf7b
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2024-12-18 09:38:57 +0000

    Moved Vertex Shader Constructor to Struct

--- SimpleEngine/Assets/Shaders/VS_BlinnPhong.hlsl
+++ SimpleEngine/Assets/Shaders/VS_BlinnPhong.hlsl
@@ -1,23 +1,19 @@
 #include "Buffers/CB0_ObjectCameraData.hlsli"
+#include "Structs/VS_BaseIn.hlsli"
 #include "Structs/VS_BaseOut.hlsli"
 
-VS_BaseOut VS_Main(
-	float3 position : POSITION,
-	float3 normal : NORMAL,
-	float2 texCoords : TEXCOORDS,
-	float3 tangent : TANGENT,
-	float3 bitangent : BITANGENT)
+VS_BaseOut VS_Main(VS_BaseIn input)
 {
     VS_BaseOut output = (VS_BaseOut) 0;
 
-    float4 Pos4 = float4(position, 1.0f);
+    float4 Pos4 = float4(input.Position, 1.0f);
     output.Position = mul(Pos4, World);
     output.WorldPosition = output.Position;
-    output.WorldNormal = normalize(mul(float4(normal, 0), World));
+    output.WorldNormal = normalize(mul(float4(input.Normal, 0), World));
     output.Position = mul(output.Position, View);
     output.Position = mul(output.Position, Projection);
-    output.Normal = float4(normal, 0);
-    output.TextureCoordinates = texCoords;
+    output.Normal = float4(input.Normal, 0);
+    output.TextureCoordinates = input.TextureCoordinates;
 
     return output;
 }
