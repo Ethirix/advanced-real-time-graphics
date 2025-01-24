6c69481c04245a29c64f98cd9d0b6740fed76f5b
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2025-01-24 00:06:18 +0000

    Further ScreenQuad Work

--- SimpleEngine/ScreenQuad.cpp
+++ SimpleEngine/ScreenQuad.cpp
@@ -1,6 +1,6 @@
 #include "ScreenQuad.h"
 
-ScreenQuad::ScreenQuad()
+ScreenQuad::ScreenQuad(const Microsoft::WRL::ComPtr<ID3D11Device>& device)
 {
 	Vertex* v = new Vertex[4];
 	
@@ -21,13 +21,30 @@ ScreenQuad::ScreenQuad()
 
 	UINT i[] = {0, 1, 2, 0, 2, 3};
 
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer{};
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer{};
+
+	D3D11_BUFFER_DESC bufferDesc = {};
+	bufferDesc.ByteWidth = sizeof(Vertex) * 4;
+	bufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+	D3D11_SUBRESOURCE_DATA subresourceData = { v };
+	device->CreateBuffer(&bufferDesc, &subresourceData, &vertexBuffer);
+
+	bufferDesc.ByteWidth = sizeof(UINT) * 6;
+	bufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	subresourceData = { i };
+	device->CreateBuffer(&bufferDesc, &subresourceData, &indexBuffer);
+
+
 	_mesh = {
 		.Name = "SSQ",
 		.Vertices = {v, 4},
 		.VertexIndices = {i, 6},
-		.SmoothShaded = ,
-		.VertexBuffer = ,
-		.IndexBuffer = ,
-		.Bounds =
+		.SmoothShaded = false,
+		.VertexBuffer = vertexBuffer,
+		.IndexBuffer = indexBuffer,
+		.Bounds = {}
 	};
 }
--- SimpleEngine/ScreenQuad.h
+++ SimpleEngine/ScreenQuad.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <wrl/client.h>
+
 #include "Mesh.h"
 
 class ScreenQuad
 {
 public:
-	ScreenQuad();
+	ScreenQuad(const Microsoft::WRL::ComPtr<ID3D11Device>& device);
+
+	void Draw(Microsoft::WRL::ComPtr<ID3D11DeviceContext> context, Microsoft::WRL::ComPtr<ID3D11RenderTargetView> rtv);
 
 private:
 	Mesh _mesh;
